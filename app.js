fe2e8a31d1b756dfe4b08561f4bf9599ae013f38
akumar228-wu <a.kumar.228@westcliff.edu>
2025-01-16 19:22:45 -0800

    lasb assignment

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+
 const app = express();
-app.set('views',path.join(__dirname, 'views'));
-app.set('views',path.join('view engine', 'pug'));
-app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
-module.exports=app;
+
+app.use(express.static('public')); 
+
+module.exports = app;
